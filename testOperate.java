cfb2ea21a8a2553e0d20a7df131d368fda401c77
Wachiraporn25 <152241126+Wachiraporn25@users.noreply.github.com>
2024-07-25 14:32:25 +0700

    Create testOperate.java

---
+++ testOperate.java
@@ -0,0 +1,14 @@
+public class testOperate {
+    public static void main(String[] args) {
+        float value;
+        int x, y;
+        x = 5 + 2 * 4;
+        y = (5 + 2) * 4;
+        System.out.println("x = " +x);
+        System.out.println("x = " +y);
+        x = x / 3;
+        System.out.println("x = " +x);
+        value = x / 3 ;
+        System.out.println("value = " +value);
+    }
+}
